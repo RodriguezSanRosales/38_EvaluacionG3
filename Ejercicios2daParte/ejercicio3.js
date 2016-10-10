a053493ec9d79a67b9cb0d853487bfcb9bee0a70
RodriguezSanRosales <san.rodzrosales@gmail.com>
2016-10-10 15:23:45 +0000

    segunda parte de evaluacion

---
+++ Ejercicios2daParte/ejercicio3.js
@@ -0,0 +1,10 @@
+// ejercicio 3
+
+function texto(palidromo){
+	var texto=prompt("ingresa el texto");
+	//en esta parte se dividiria las palabras con /\s/ para ver los espacios
+	for(i=0; i<=texto.lenght; i++){ //se cuentan las letras para despues acomodarlas
+
+	}
+
+}
---
+++ Ejercicios2daParte/ejercicio4.js
@@ -0,0 +1,20 @@
+//ejercicio4
+
+var persona={
+	nombre: ,
+	edad: ,
+	genero: ,
+	this.obtDetalles=function obtDetalles(){
+    //
+	} 
+
+};
+var estudiante={
+	curso: ,
+	grupo: ,
+};
+var profesor={
+	asignatura: ,
+	nivel: 
+};
+//creo si necesito repasar muuucho más
